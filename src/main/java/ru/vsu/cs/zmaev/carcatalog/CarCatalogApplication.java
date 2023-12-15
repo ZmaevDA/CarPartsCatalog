c67fa5ed6eb128fb9f673d6a75c2a682a15bc09b
dany0k <zmaev.vsu@gmail.com>
2023-12-15 16:55:42 +0300

    feat: first commit

---
+++ src/main/java/ru/vsu/cs/zmaev/carcatalog/CarCatalogApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.zmaev.carcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarCatalogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vsu/cs/zmaev/carcatalog/CarCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.zmaev.carcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
