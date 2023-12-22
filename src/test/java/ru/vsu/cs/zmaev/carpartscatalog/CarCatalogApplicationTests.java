eaa2df54a0ba5f535104ba88a85a436835ea17ea
dany0k <zmaev.vsu@gmail.com>
2023-12-22 17:33:10 +0300

    fix: update deploy

--- src/test/java/ru/vsu/cs/zmaev/carpartscatalog/CarCatalogApplicationTests.java
+++ src/test/java/ru/vsu/cs/zmaev/carpartscatalog/CarCatalogApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CarCatalogApplicationTests {
+class CarPartsCatalogApplicationTests {
 
     @Test
     void contextLoads() {
